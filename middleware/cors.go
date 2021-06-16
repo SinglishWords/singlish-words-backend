0b607f442c11bfccf245b759c4d2b72ad473d8fc
gongzq5 <gongzq5@foxmail.com>
2021-06-16 17:19:31 +0800

    cors set to *, add a max age 600

--- middleware/cors.go
+++ middleware/cors.go
@@ -6,13 +6,17 @@ import (
 
 func Cors() gin.HandlerFunc {
 	return func(c *gin.Context) {
-		//method := c.Request.Method
-
-		c.Header("Access-Control-Allow-Origin", "localhost:3000")
-		c.Header("Access-Control-Allow-Methods", "POST, GET, PATCH, PUT, DELETE, UPDATE")
+		c.Header("Access-Control-Allow-Origin", "*")
+		c.Header("Access-Control-Allow-Methods", "POST, GET, OPTIONS, PATCH, PUT, DELETE, UPDATE")
 		c.Header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Authorization")
+		c.Header("Access-Control-Max-Age", "600")
 		c.Header("Access-Control-Expose-Headers", "Content-Length, Access-Control-Allow-Origin, Access-Control-Allow-Headers, Cache-Control, Content-Language, Content-Type")
 		//c.Header("Access-Control-Allow-Credentials", "true")
+
+		if c.Request.Method == "OPTIONS" {
+            c.AbortWithStatus(204)
+            return
+        }
 
 		c.Next()
 	}
