fa24c456717aab33476b65452b7d5ce44afe024e
Shashank Acharya <acharya.s@icloud.com>
2022-03-10 22:07:59 +0800

    fixed update bug

--- setup_db.sh
+++ setup_db.sh
@@ -1,6 +1,7 @@
 # Read Password
 read -s -p "MySQL Password: " password
 # Run Command
-mysql -uroot -p$password < sql/initDB.sql < sql/questions-test-data.sql
-perl -pi -e 's/csqsiew/root/g' ./config.yaml
-perl -pi -e 's/123456/$password/g' ./config.yaml
+mysql -uroot -p$password < sql/initDB.sql
+mysql -uroot -p$password < sql/questions-test-data.sql
+perl -pi -e "s/csqsiew/root/g" ./config.yaml
+perl -pi -e "s/123456/$password/g" ./config.yaml
