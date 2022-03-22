07158d740e41b46cf001f2b9aca23c446b559fb0
Shashank Acharya <acharya.s@icloud.com>
2022-03-22 22:50:38 +0800

    added count

--- model/question.go
+++ model/question.go
@@ -6,6 +6,7 @@ type Question struct {
 	Id   int64   `json:"id" db:"id"`
 	Word string  `json:"word" db:"word"`
 	Enable int64 `json:"enable" db:"enable"`
+	Count int64  `json:"count" db:"count"`
 }
 
 func (q Question) MarshalBinary() (data []byte, err error) {
