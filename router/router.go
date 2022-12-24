4f98b6b662c900c89213cbe6421909d933f5991f
Kevin Mingtarja <kevin.mingtarja@u.nus.edu>
2022-12-24 00:04:07 -0500

    Merge /association/random API into one

--- router/router.go
+++ router/router.go
@@ -40,8 +40,7 @@ func InitRouter(g *gin.Engine) *gin.Engine {
 		// Association
 		v1.GET("/associations/forward/:word", responseWrapper(association.GetForwardAssociations))
 		v1.GET("/associations/backward/:word", responseWrapper(association.GetBackwardAssociations))
-		v1.GET("/associations/random/forward", responseWrapper(association.GetRandomWordForwardAssociations))
-		v1.GET("/associations/random/backward", responseWrapper(association.GetRandomWordBackwardAssociations))
+		v1.GET("/associations/random", responseWrapper(association.GetRandomWordAssociations))
 	}
 
 	return g
