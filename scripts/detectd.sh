f83053979cbba0fc1211d0b2852147d23f916fce
kevin <kevin.mingtarja@gmail.com>
2023-01-12 06:58:48 +0000

    refactor detectd script

--- scripts/detectd.sh
+++ scripts/detectd.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 
-echo $(date +"%T") >> detect-categories.log
-./scripts detect-categories | head >> detect-categories.log
+echo $(date +"%T") >> ~/detect-categories.log
+cd /root/deployment/backend/src/scripts
+./scripts detect-categories | head >> ~/detect-categories.log
